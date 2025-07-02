1f031ba69b371265fa0397b80a25c6a8ca13d09d
yanet coronel <ya.coronel@gmail.com>
2025-07-02 17:39:29 -0300

    strings

---
+++ strings.py
@@ -0,0 +1,13 @@
+quotes = "This is a string."
+apostrophes = 'This is also a string.'
+
+print(quotes)
+print(apostrophes)
+
+string1 = 'I told my friend,"Python is my favorite language!"'
+string2 = "The language 'Python' is named after Monty Python, not the snake"
+string3 = "One of Python's strengths is its diverse and supportive community"
+
+print(string1)
+print(string2)
+print(string3)
