9e2baff6a4eac7ed22a4a3be9e35ace03b94bd4a
yanet coronel <ya.coronel@gmail.com>
2025-07-04 20:02:57 -0300

    Uso de Jupyter notebooks

--- python_files/whitespace.py
+++ python_files/whitespace.py
@@ -1,8 +1,11 @@
 print("Python")
 
+# We use \t to add a tab to the text
 print("\tPython")
 
+# We use \n to add a new line
 print("Languages: \nPython\nC\nJavaScript")
 
+# \n\t adds a new line and starts the text after a tab
 print("Languages: \n\tPython\n\tC\n\tJavaScript")
 
